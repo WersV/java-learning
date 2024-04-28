9b01471504e32c57d683032971bca3b15c120bfa
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-04-28 10:46:20 +0200

    homework done

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Cześć "+ name);
+        System.out.println("Podaj liczbę a:");
+        int aNum = scanner.nextInt();
+        System.out.println("Podaj liczbę b:");
+        int bNum = scanner.nextInt();
+
+        System.out.println("Dodawanie: "+ (aNum+bNum));
+        System.out.println("Odejmowanie: " + (aNum-bNum));
+        System.out.println("Mnożenie: " + (aNum*bNum));
+        System.out.println("Dzielenie: " + (aNum/bNum));
     }
 }
