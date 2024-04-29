d3e9897621f678edd856386aee90dc3360a7e2cb
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-04-29 13:44:10 +0200

    Student class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,44 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę a:");
-        int aNum = scanner.nextInt();
-        System.out.println("Podaj liczbę b:");
-        int bNum = scanner.nextInt();
+        Student student1 = new Student();
+        student1.name = "Przemek";
+        student1.surname = "Botwina";
+        student1.email = "qwe@w.pl";
+        student1.nick = "WersV";
+        student1.indexId = 990;
 
-        System.out.println("Dodawanie: "+ (aNum+bNum));
-        System.out.println("Odejmowanie: " + (aNum-bNum));
-        System.out.println("Mnożenie: " + (aNum*bNum));
-        System.out.println("Dzielenie: " + (aNum/bNum));
+
+        Student student2 = new Student();
+        student2.name = "Jan";
+        student2.surname = "Kowalski";
+        student2.email = "adsasd@sad.pl";
+        student2.nick = "JANEK";
+        student2.indexId = 119;
+
+
+        Student student3 = new Student();
+        student3.name = "Zdzisław";
+        student3.surname = "Nowak";
+        student3.email = "bnbnb@vvc.pl";
+        student3.nick = "Zdziśek";
+        student3.indexId = 201;
+
+        Student[] studentsArr = new Student[3];
+
+        studentsArr[0] = student1;
+        studentsArr[1] = student2;
+        studentsArr[2] = student3;
+
+
+
+//        for (Student student : studentsArr) {
+//            student.sayHello();
+//            student.login();
+//            student.provideIndexId();
+//            student.provideEmail();
+//            System.out.println("=============");
+//        }
+        int returnedValue = student3.add(3,5);
+        System.out.println("Suma to: " + returnedValue);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -8,8 +8,24 @@ public class Student {
     public void sayHello() {
         System.out.println(name + " " + surname + " says hello!");
     }
+
     public void login() {
-        System.out.println(email + " " + indexId + " logged in!");
+        System.out.println(nick + " logged in!");
     }
 
+    public void provideIndexId() {
+        System.out.println("Index number: " + indexId);
+    }
+
+    public void provideEmail() {
+        System.out.println("Email: " + email);
+    }
+
+    public int doCalc() {
+        return 4;
+    }
+
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
 }
