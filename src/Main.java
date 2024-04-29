85fe33e7a4597bc0e4e36b4fb0a3f12928773d1a
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-04-29 15:12:27 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę a:");
-        int aNum = scanner.nextInt();
-        System.out.println("Podaj liczbę b:");
-        int bNum = scanner.nextInt();
 
-        System.out.println("Dodawanie: "+ (aNum+bNum));
-        System.out.println("Odejmowanie: " + (aNum-bNum));
-        System.out.println("Mnożenie: " + (aNum*bNum));
-        System.out.println("Dzielenie: " + (aNum/bNum));
     }
 }
