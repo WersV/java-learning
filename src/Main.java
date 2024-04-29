87d16a0734c69eac2bd1a2636125b7a08a8de70d
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-04-29 17:57:05 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Auto mercedes = new Auto("Mercedes", "S-Class", 2024, 1000);
-
-        mercedes.drive();
-        mercedes.info();
-        System.out.println(Auto.country);
     }
 }
