cbab455eb4459d3c8c92ec869839b69114359de4
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-08 22:25:48 +0200

    learn throws

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        readFile("text.txt");
+    }
+
+    public static void readFile(String path) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(path);
     }
 }
