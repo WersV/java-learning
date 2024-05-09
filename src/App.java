0a9c03648f4467088e19981ae87b965c23f1dd9a
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-09 20:29:47 +0200

    init

--- src/App.java
+++ src/App.java
@@ -5,18 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws InvalidException {
-        getAge();
-    }
+    public static void main(String[] args) {
 
-    public static void getAge() throws InvalidException{
-        System.out.println("Podaj wiek: ");
-        Scanner scanner = new Scanner(System.in);
-        int age = scanner.nextInt();
-        if(age<0) {
-            throw new InvalidException("Wiek nie może być mniejszy od 0");
-        } else if(age >= 18) {
-            System.out.println("Jesteś pełnoletni");
-        }
     }
 }
--- src/exceptions/InvalidException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidException extends Exception{
-    public InvalidException(String exception) {
-        super(exception);
-    }
-}
