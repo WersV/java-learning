acc29b4ca65acde54a6b803e5cb406418bccc930
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-10 20:18:29 +0200

    learn files

--- src/App.java
+++ src/App.java
@@ -1,9 +1,3 @@
-import exceptions.InvalidException;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
 
---
+++ src/files/WriteFile.java
@@ -0,0 +1,12 @@
+package files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("test.txt");
+        writer.write("test text");
+        writer.close();
+    }
+}
