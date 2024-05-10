8e0643ea5ce49ad0e76e0e5a965705083ef32f51
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-10 20:52:38 +0200

    learn basic enums

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import enums.DriverType;
+
 public class App {
     public static void main(String[] args) {
+        getDriver(DriverType.FIREFOX);
+    }
 
+    public static void getDriver(DriverType type) {
+        if(type == DriverType.CHROME) {
+            System.out.println("Korzystasz z chrome");
+        }
+        if(type == DriverType.FIREFOX) {
+            System.out.println("Korzystasz z firefoxa");
+        }
     }
 }
