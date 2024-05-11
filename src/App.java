3438b4791b06e27f1fdf27be1494901b1bf8d6d8
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-11 15:52:48 +0200

    learn lambda expression

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
 public class App {
     public static void main(String[] args) {
 
+        Student jerry = new Student() {
+            @Override
+            public void sayHello(String name) {
+                System.out.println("Hi I'm " + name);
+            }
+        };
+        sayHello("jerry", jerry);
+
+        //second way with lambda expression
+        Student tom = name -> System.out.println("Hi I'm " + name);
+        sayHello("Tom", tom);
     }
 
+    public static void sayHello(String name, Student student) {
+        student.sayHello(name);
+    }
 }
