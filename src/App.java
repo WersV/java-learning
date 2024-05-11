767287bdf4ff492a1a9abf1d0733f45bb03ec6ea
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-11 15:09:21 +0200

    init

--- src/App.java
+++ src/App.java
@@ -1,16 +1,6 @@
-import enums.DriverType;
-
 public class App {
     public static void main(String[] args) {
-        getDriver(DriverType.CHROME);
+
     }
 
-    public static void getDriver(DriverType type) {
-        if(type.name.equals("chrome")) {
-            System.out.println("Korzystasz z chrome");
-        }
-        if(type.name.equals("firefox")) {
-            System.out.println("Korzystasz z firefoxa");
-        }
-    }
 }
